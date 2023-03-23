8070a51f38bf1ac99e48c42c7735e03e09c3372a
Zohair <zohairalsheikh2002@gmail.com>
2023-03-23 12:10:47 -0500

    More work

--- client.cpp
+++ client.cpp
@@ -6,12 +6,144 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <sstream>
+
+/*
+struct info
+{
+    HFTree tree;
+    std::string line;
+    std::string code;
+    std::vector<char> *message;
+    std::vector<std::pair<int, char>> decoded;
+    std::vector<int> pos;
+    info(const HFTree& tree, const std::string& line)
+        : tree(tree), line(line)
+    {}
+};
 
 
+std::vector<info*> retrieve_lines(std::string entered, int &count)
+{
+    std::ifstream file;
+    std::string line;
+    file.open(entered);
+    while (std::getline(file, line))
+    {
+        info *entry = new info(tree, line);
+        data.push_back(entry);
+        count++;
+    }
+
+    file.close();
+
+    return data;
+}
+
+std::string retrieve_code(std::string line)
+{
+    std::string returned;
+    while(line[0] != ' ')
+    {
+        returned.push_back(line[0]);
+        line.erase(0,1);
+    }
+    line.erase(0, 1);
+    return returned;
+}
+
+std::vector<int> retrieve_pos(std::string line, std::vector<int> pos, int num)
+{
+    std::string curr;
+    std::stringstream ss(line);
+    while (getline(ss, curr, ' '))
+    {
+        pos.push_back(std::stoi(curr));
+    }
+    
+    return pos;
+}
+
+Node* search(std::string code, Node* root, int count)
+{
+    if (!root)
+    {
+        return nullptr;
+    }
+
+    if (count == code.length())
+    {
+        return root;
+    }
+
+
+    switch (code[count])
+    {
+        case '0':
+            return search(code, root->getLNode(), count+1);
+        case '1':
+            return search(code, root->getRNode(), count+1);
+    }
+    return nullptr;
+}
+
+info *make_pairs(info *&ptr, Node *node)
+{
+    for (int i = 0; i < ptr->pos.size(); i++)
+    {
+        ptr->decoded.push_back(std::make_pair(ptr->pos.at(i), node->getChar()));
+    }
+
+    return ptr;
+}
+
+
+void *decode(void *void_ptr)
+{
+    info *pos_ptr = (info*)void_ptr;
+    pos_ptr->code = retrieve_code(pos_ptr->line);
+    pos_ptr->line.erase(0,pos_ptr->code.length()+1);
+    pos_ptr->pos = retrieve_pos(pos_ptr->line, pos_ptr->pos, pos_ptr->code.length());
+    int count = 0;
+    Node *symbol_node = search(pos_ptr->code, pos_ptr->tree.getRoot(), count);
+    make_pairs(pos_ptr, symbol_node);
+    for (int i = 0; i < pos_ptr->decoded.size(); i++)
+    {
+        int position = pos_ptr->decoded.at(i).first;
+        pos_ptr->message->at(position) = pos_ptr->decoded.at(i).second;
+    };
+    
+    return nullptr;
+}
+
+*/
 int main(int argc, char *argv[])
 {
+    std::string entered;
+    std::cin >> entered;
+    int threads = 0;
+
+    pthread_t *tid = new pthread_t[threads];
+    std::ifstream file;
+
+    /*
+    for (int i = 0; i < threads; i++)
+    {
+        datatodecode.at(i)->message = &originalMSG;
+        if(pthread_create(&tid[i], NULL, decode, (void*)datatodecode.at(i)))
+        {
+            fprintf(stderr, "Error occured\n");
+            return 1;
+        }
+        wait(nullptr);
+    }
+    */
     int sockfd, portno, n;
 
     struct sockaddr_in serv_addr;
@@ -46,17 +178,17 @@ int main(int argc, char *argv[])
     int sizeofmessage;
     std::cout << "Please input size of message: ";
     std::cin >> sizeofmessage;
-    std::cout << "Please input message: ";  
+    std::cout << "Please input message: ";
+    
     char message[sizeofmessage];
     std::cin >> message;
-    int sMessage = strlen(message);
-    n = write(sockfd, &sMessage, sizeof(int));
+    n = write(sockfd, &sizeofmessage, sizeof(int));
     if (n < 0)
     {
         std::cerr << "ERROR writing to socket";
         exit(1);
     }
-    n = write(sockfd, message, sMessage);
+    n = write(sockfd, message, sizeofmessage);
     if (n < 0)
     {
         std::cerr << "ERROR writing to socket";
--- server.cpp
+++ server.cpp
@@ -13,6 +13,211 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/wait.h>
+#include <vector>
+#include <sstream>
+#include <queue>
+#include <fstream>
+
+
+class Node
+{
+    private:
+        char data;
+        int freq;
+        int id;
+        std::string code;
+        Node *left, *right;
+    public:
+        Node(char, int);
+
+        int getFreq()
+        {
+            return freq;
+        }
+
+        int setFreq(int new_var)
+        {
+            return this->freq = new_var;
+        }
+
+        char getChar()
+        {
+            return data;
+        }
+
+        char setChar(char new_var)
+        {
+            return this->data = new_var;
+        }
+
+        int getID()
+        {
+            return id;
+        }
+
+        int setID(int new_var)
+        {
+            return this->id = new_var;
+        }
+
+        std::string getCode()
+        {
+            return code;
+        }
+
+        std::string setCode(std::string new_var)
+        {
+            return this->code = new_var;
+        }
+
+        Node* getLNode()
+        {
+            return left;
+        }
+
+        Node* setLNode(Node* new_var)
+        {
+            return this->left = new_var;
+        }
+
+        Node * getRNode()
+        {
+            return right;
+        }
+
+        Node* setRNode(Node* new_var)
+        {
+            return this->right = new_var;
+        }
+};
+
+
+
+Node::Node(char data, int freq)
+{
+    this->data = data;
+    this->freq = freq;
+    left = right = nullptr;
+}
+
+class compare
+{
+    public:
+        bool operator()(Node *left, Node *right)
+        {
+            if (left->getFreq() == right->getFreq())
+            {
+                if (left->getChar() == right->getChar())
+                {
+                    return (left->getID() < right->getID());
+                }
+                return (left->getChar() > right->getChar());
+            }
+            return (left->getFreq() > right->getFreq());
+        }
+};
+
+class HFTree
+{
+    private:
+        Node *root;
+    public:
+        HFTree(std::string);
+        void display(Node*, std::string);
+        Node* getRoot()
+        {
+            return root;
+        }
+};
+
+Node* search(std::string code, Node* root, int count)
+{
+    if (!root)
+    {
+        return nullptr;
+    }
+
+    if (count == code.length())
+    {
+        return root;
+    }
+
+
+    switch (code[count])
+    {
+        case '0':
+            return search(code, root->getLNode(), count+1);
+        case '1':
+            return search(code, root->getRNode(), count+1);
+    }
+    return nullptr;
+}
+
+HFTree::HFTree(std::string name)
+{
+    std::vector<std::pair<char, int>> parsedinfo;
+    std::ifstream file;
+    std::string test;
+    file.open(name);
+    while(std::getline(file, test))
+    {
+        char symbol = test[0];
+        int freq = test[2]-'0';
+        parsedinfo.emplace_back(std::make_pair(symbol, freq));
+    }
+    file.close();
+
+    std::priority_queue<Node*, std::vector<Node*>, compare> pq;
+
+    for(int i=0; i < parsedinfo.size(); i++)
+    {
+        pq.push(new Node(parsedinfo.at(i).first, parsedinfo.at(i).second));
+    }
+
+    int place = 0;
+    while (pq.size() != 1)
+    {
+        Node *left = pq.top();
+        pq.pop();
+
+        Node *right= pq.top();
+        pq.pop();
+
+        Node *temp = new Node('\0', left->getFreq() + right->getFreq());
+        temp->setLNode(left);
+        temp->setRNode(right);
+        temp->setID(place);
+        place++;
+
+        pq.push(temp);
+
+    }
+
+    this->root = pq.top();
+
+    display(root, "");
+}
+
+void HFTree::display(Node *root, std::string str)
+{
+    if (!root)
+    {
+        return;
+    }
+
+    root->setCode(str);
+
+    if (root->getChar() != '\0')
+    {
+        std::cout << "Symbol: " << root->getChar() << ", Frequency: " << root->getFreq() << ", Code: " << root->getCode() << std::endl;
+    }
+
+    display(root->getLNode(), str + "0");
+    display(root->getRNode(), str + "1");
+}
+
+
+
 
 void fireman(int)
 {
@@ -21,6 +226,9 @@ void fireman(int)
 
 int main(int argc, char *argv[])
 {
+    std::string enteredone;
+    std::cin >> enteredone;
+    HFTree root = HFTree(enteredone);
     int sockfd, newsockfd, portno, clilen;
     struct sockaddr_in serv_addr, cli_addr;
     int n;
