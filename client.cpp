965b9619a345d2f4969a0110242f864ef972bae6
AridArab <zohairalsheikh2002@gmail.com>
2023-03-28 16:39:48 -0500

    Did more work

--- client.cpp
+++ client.cpp
@@ -13,152 +13,62 @@
 #include <vector>
 #include <queue>
 #include <sstream>
+#include <cstring>
 
-/*
-struct info
+struct package
 {
-    HFTree tree;
+    char* hostname;
+    int port;
     std::string line;
-    std::string code;
+    char symbol;
     std::vector<char> *message;
-    std::vector<std::pair<int, char>> decoded;
-    std::vector<int> pos;
-    info(const HFTree& tree, const std::string& line)
-        : tree(tree), line(line)
-    {}
+
+    
 };
 
 
-std::vector<info*> retrieve_lines(std::string entered, int &count)
+
+std::vector<std::string> retrieve_lines(std::string entered, std::vector<package*> &data, int &count, int &msgsize)
 {
     std::ifstream file;
     std::string line;
+    std::vector<std::string> lines;
     file.open(entered);
     while (std::getline(file, line))
     {
-        info *entry = new info(tree, line);
+        package *entry = new package();
         data.push_back(entry);
+        data.at(count)->line = line;
+        size_t pos = line.find_last_of(" ");
+        std::string last = line.substr(pos + 1);
+        int last_num = atoi(last.c_str());
+        if (last_num > msgsize)
+        {
+            msgsize = last_num;
+        }
         count++;
     }
 
     file.close();
 
-    return data;
+    return lines;
 }
 
-std::string retrieve_code(std::string line)
+void *child_thread(void *void_ptr)
 {
-    std::string returned;
-    while(line[0] != ' ')
-    {
-        returned.push_back(line[0]);
-        line.erase(0,1);
-    }
-    line.erase(0, 1);
-    return returned;
-}
-
-std::vector<int> retrieve_pos(std::string line, std::vector<int> pos, int num)
-{
-    std::string curr;
-    std::stringstream ss(line);
-    while (getline(ss, curr, ' '))
-    {
-        pos.push_back(std::stoi(curr));
-    }
-    
-    return pos;
-}
-
-Node* search(std::string code, Node* root, int count)
-{
-    if (!root)
-    {
-        return nullptr;
-    }
-
-    if (count == code.length())
-    {
-        return root;
-    }
-
-
-    switch (code[count])
-    {
-        case '0':
-            return search(code, root->getLNode(), count+1);
-        case '1':
-            return search(code, root->getRNode(), count+1);
-    }
-    return nullptr;
-}
-
-info *make_pairs(info *&ptr, Node *node)
-{
-    for (int i = 0; i < ptr->pos.size(); i++)
-    {
-        ptr->decoded.push_back(std::make_pair(ptr->pos.at(i), node->getChar()));
-    }
-
-    return ptr;
-}
-
-
-void *decode(void *void_ptr)
-{
-    info *pos_ptr = (info*)void_ptr;
-    pos_ptr->code = retrieve_code(pos_ptr->line);
-    pos_ptr->line.erase(0,pos_ptr->code.length()+1);
-    pos_ptr->pos = retrieve_pos(pos_ptr->line, pos_ptr->pos, pos_ptr->code.length());
-    int count = 0;
-    Node *symbol_node = search(pos_ptr->code, pos_ptr->tree.getRoot(), count);
-    make_pairs(pos_ptr, symbol_node);
-    for (int i = 0; i < pos_ptr->decoded.size(); i++)
-    {
-        int position = pos_ptr->decoded.at(i).first;
-        pos_ptr->message->at(position) = pos_ptr->decoded.at(i).second;
-    };
-    
-    return nullptr;
-}
-
-*/
-int main(int argc, char *argv[])
-{
-    std::string entered;
-    std::cin >> entered;
-    int threads = 0;
-
-    pthread_t *tid = new pthread_t[threads];
-    std::ifstream file;
-
-    /*
-    for (int i = 0; i < threads; i++)
-    {
-        datatodecode.at(i)->message = &originalMSG;
-        if(pthread_create(&tid[i], NULL, decode, (void*)datatodecode.at(i)))
-        {
-            fprintf(stderr, "Error occured\n");
-            return 1;
-        }
-        wait(nullptr);
-    }
-    */
+    package *pos_ptr = (package*)void_ptr;
     int sockfd, portno, n;
+    char sendmsg[pos_ptr->line.size() + 1];
+    std::strcpy(sendmsg, pos_ptr->line.c_str());
+
 
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    if (argc < 3)
-    {
-        std::cerr << "usage " << argv[0] << "hostname port\n";
-        exit(0);
-    }
-    portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         std::cerr << "ERROR opening socket";
-    server = gethostbyname(argv[1]);
+    server = gethostbyname(pos_ptr->hostname);
     if (server == NULL)
     {
         std::cerr << "ERROR, no such host\n";
@@ -169,26 +79,21 @@ int main(int argc, char *argv[])
     bcopy((char *)server->h_addr,
           (char *)&serv_addr.sin_addr.s_addr,
           server->h_length);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(pos_ptr->port);
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         std::cerr << "ERROR connecting";
         exit(1);
     }
-    int sizeofmessage;
-    std::cout << "Please input size of message: ";
-    std::cin >> sizeofmessage;
-    std::cout << "Please input message: ";
-    
-    char message[sizeofmessage];
-    std::cin >> message;
+
+    int sizeofmessage = pos_ptr->line.size();
     n = write(sockfd, &sizeofmessage, sizeof(int));
     if (n < 0)
     {
         std::cerr << "ERROR writing to socket";
         exit(1);
     }
-    n = write(sockfd, message, sizeofmessage);
+    n = write(sockfd, sendmsg, sizeof(sendmsg));
     if (n < 0)
     {
         std::cerr << "ERROR writing to socket";
@@ -207,5 +112,44 @@ int main(int argc, char *argv[])
     std::cout << buffer << std::endl;
     delete [] buffer;
     close(sockfd);
+    
+    return nullptr;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    std::string entered;
+    std::cin >> entered;
+    std::vector<std::string> inputed;
+    std::vector<package*> datatobesent;
+    int threads = 0;
+    int msgsize = 0;
+    inputed = retrieve_lines(entered, datatobesent, threads, msgsize);
+    std::vector<char> finalmsg(msgsize);
+    std::cout << "Size of final msg: " << finalmsg.size() << std::endl;
+    pthread_t *tid = new pthread_t[threads];
+    
+
+
+    for (int i = 0; i < threads; i++)
+    {
+        datatobesent.at(i)->message = &finalmsg;
+        datatobesent.at(i)->port = atoi(argv[2]);
+        datatobesent.at(i)->hostname = argv[1];
+        if(pthread_create(&tid[i], NULL, child_thread, (void*)datatobesent.at(i)))
+        {
+            fprintf(stderr, "Error occured\n");
+            return 1;
+        }
+        wait(nullptr);
+    }
+
+    for (int i = 0; i < threads; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+
     return 0;
 }
